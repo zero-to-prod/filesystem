dc2a9859baae50c0ce6715458a22bf0388f2b7b8
david_smith <david_smith@sweetwater.com>
2025-02-16 13:13:04 -0500

    feat/filesystem Updated `src/Filesystem.php`.

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -9,6 +9,21 @@ use SplFileInfo;
 
 class Filesystem
 {
+
+    public static function getFilesByExtension(string $directory, string $extension): array
+    {
+        $files = [];
+
+        /** @var SplFileInfo $SplFileInfo */
+        foreach (new DirectoryIterator($directory) as $SplFileInfo) {
+            if (!$SplFileInfo->isDot() && !$SplFileInfo->isDir() && $SplFileInfo->getExtension() === $extension) {
+                $files[$SplFileInfo->getFilename()] = $SplFileInfo;
+            }
+        }
+
+        return $files;
+    }
+
     public static function getFilesByExtensionRecursive(string $directory, string $extension): array
     {
         $files = [];
@@ -19,20 +34,6 @@ class Filesystem
         /** @var SplFileInfo $fileInfo */
         foreach ($RecursiveIteratorIterator as $SplFileInfo) {
             if (!$SplFileInfo->isDir() && $SplFileInfo->getExtension() === $extension) {
-                $files[$SplFileInfo->getFilename()] = $SplFileInfo;
-            }
-        }
-
-        return $files;
-    }
-
-    public static function getFilesByExtension(string $directory, string $extension): array
-    {
-        $files = [];
-
-        /** @var SplFileInfo $SplFileInfo */
-        foreach (new DirectoryIterator($directory) as $SplFileInfo) {
-            if (!$SplFileInfo->isDot() && !$SplFileInfo->isDir() && $SplFileInfo->getExtension() === $extension) {
                 $files[$SplFileInfo->getFilename()] = $SplFileInfo;
             }
         }
